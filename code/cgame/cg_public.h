bc2e495d5423add58a5cb29595f1d33e805f1c39
Brian Cullinan <megamindbrian@gmail.com>
2024-08-08 16:45:33 -0700

    not enough items loading in a single snapshot

--- code/cgame/cg_public.h
+++ code/cgame/cg_public.h
@@ -29,7 +29,11 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 // needs to be larger than PACKET_BACKUP
 
 
+#if defined(USE_MULTIVM_CLIENT) || defined(USE_HORDES)
+#define	MAX_ENTITIES_IN_SNAPSHOT	MAX_GENTITIES
+#else
 #define	MAX_ENTITIES_IN_SNAPSHOT	256
+#endif
 
 // snapshots are a view of the server at a given time
 
